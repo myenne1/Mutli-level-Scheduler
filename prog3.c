ec4f8fe922bbae0b9c7a6d163ec6511e1e0c502d
myenne1 <yennesmuhanad@gmail.com>
2025-04-04 23:22:21 -0500

    Implemented queue1 logic, blocked processes and I/O processing.

--- prog3.c
+++ prog3.c
@@ -1,60 +1,140 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "prioque.h"
+
+#define MAX_BLOCKED 11
 
 typedef struct _Process
 {
     int time;
     int pid;
-    int run;
+    int originalRunTime;
+    int remainingRunTime;
     int io;
     int repeat;
+    int ioEndTime;
 } Process;
+
+// Globals
+Process processes[11];
+Process blockedProcesses[MAX_BLOCKED];
+Queue blockedProcesses;
+Queue q1, q2, q3, q4;
+int runningProcessCount = 0;
+int currentTime = 0;
+int blockedCount = 0;
+
+void processQ1()
+{
+    int q = 10;
+    int b = 1;
+    Process p;
+
+    remove_from_front(&q1, &p);
+
+    if (p.io == 0 && p.repeat == 0 && p.remainingRunTime == 0)
+    {
+        runningProcessCount -= 1;
+        printf("FINISHED: Process %d finished at time %d.\n", p.pid, currentTime);
+    }
+    else
+    {
+        if (p.remainingRunTime <= q)
+        {
+            printf("RUN: Process %d started execution from level 1 at time %d; wants to execute for %d ticks.\n", p.pid, currentTime, p.remainingRunTime);
+            p.remainingRunTime -= q;
+            currentTime += q;
+            p.ioEndTime = currentTime + p.io;
+
+            if (blockedCount < MAX_BLOCKED)
+            {
+                blockedProcesses[blockedCount] = p;
+                blockedCount++;
+                printf("I/O: Process %d blocked for I/O at time %d.\n", p.pid, currentTime);
+            }
+            else
+            {
+                printf("Blocked Processes array reached limit\n");
+            }
+        }
+        else
+        {
+            p.remainingRunTime -= q;
+            currentTime += q;
+
+            add_to_queue(&q2, &p, p.pid);
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    Process processes[11];
-    Queue q1, q2, q3, q4;
-    Queue readyQueue1, readyQueue2, readyQueue3, readyQueue4;
+    Queue readyQueue;
+    Process p;
+    int count = 0;
 
     printf("Queue entries one per line: \n");
-    int count = 0;
+
     while (scanf("%d %d %d %d %d", &processes[count].time,
                  &processes[count].pid,
-                 &processes[count].run,
+                 &processes[count].originalRunTime,
                  &processes[count].io,
                  &processes[count].repeat) == 5)
     {
         count++;
+        runningProcessCount++;
     }
 
-    int currentTime = 0;
-
     // Initialize queues
-    init_queue(&readyQueue1, sizeof(Queue), TRUE, NULL, TRUE); // readyQueue1 = highest priority
-    init_queue(&readyQueue2, sizeof(Queue), TRUE, NULL, TRUE);
-    init_queue(&readyQueue3, sizeof(Queue), TRUE, NULL, TRUE);
-    init_queue(&readyQueue4, sizeof(Queue), TRUE, NULL, TRUE);
+    init_queue(&readyQueue, sizeof(Process), TRUE, NULL, FALSE);
 
-    init_queue(&q1, sizeof(Queue), TRUE, NULL, FALSE); // q1 = highest priority
-    init_queue(&q2, sizeof(Queue), TRUE, NULL, FALSE);
-    init_queue(&q3, sizeof(Queue), TRUE, NULL, FALSE);
-    init_queue(&q4, sizeof(Queue), TRUE, NULL, FALSE);
+    init_queue(&q1, sizeof(Process), TRUE, NULL, FALSE); // q1 = highest priority
+    init_queue(&q2, sizeof(Process), TRUE, NULL, FALSE);
+    init_queue(&q3, sizeof(Process), TRUE, NULL, FALSE);
+    init_queue(&q4, sizeof(Process), TRUE, NULL, FALSE);
 
     // Start simulation
-    while (count != 0)
+    while (runningProcessCount != 0)
     {
-        for (int i = 0; i < count; i++)
+        for (int i = 0; i < count; i++) // checks if any processes arrived
         {
-            if (processes[i].time == currentTime)
+            if (processes[i].time <= currentTime)
             {
                 printf("PID: %d Arrival Time: %d\n", processes[i].pid, currentTime);
-                add_to_queue(&readyQueue1, &processes[i], processes[i].pid);
-                printf("CREATE: Process %d entered the ready queue at time %d\n", processes[i].pid, currentTime);
+                add_to_queue(&readyQueue, &processes[i], processes[i].pid);
+                printf("CREATE: Process %d entered the ready queue at time %d.\n", processes[i].pid, currentTime);
+
+                for (int j = i; j < count - 1; j++)
+                { // Removes process from processes array by shifting all elements after it to the left
+                    processes[j] = processes[j + 1];
+                }
+                count--;
+                i--;
             }
         }
 
+        for (int i = 0; i < blockedCount; i++)
+        {
+            if (blockedProcesses[i].ioEndTime <= currentTime)
+            {
+                blockedProcesses[i].remainingRunTime = blockedProcesses[i].originalRunTime; // Reset run time for repeat
+                blockedProcesses[i].repeat -= 1;
+                add_to_queue(&q1, &blockedProcesses[i], &blockedProcesses[i].pid);
+
+                for (int j = i; j < blockedCount - 1; j++)
+                { // Removes process from blockedProcesses array by shifting all elements after it to the left
+                    blockedProcesses[j] = blockedProcesses[j + 1];
+                }
+                blockedCount--;
+            }
+        }
+        if (!empty_queue(&readyQueue)) {
+            remove_from_front(&readyQueue, &p);
+            add_to_queue(&q1, &p, p.pid);
+            processQ1();
+        }
         currentTime++;
     }
 }
